b84a40e166101b294d6b403937810bb49c711390
y h <489693132@qq.com>
2023-09-26 08:05:31 -0700

    request

--- src/main.py
+++ src/main.py
@@ -89,8 +89,8 @@ class AlleyServer(asyncssh.SSHServer):
     def validate_password(self, username: str, password: str) -> MaybeAwait[bool]:
         return username == 'guest'
     
-    # def connection_requested(self, dest_host: str, dest_port: int, orig_host: str, orig_port: int) -> _NewTCPSession:
-    #     return super().connection_requested(dest_host, dest_port, orig_host, orig_port)
+    def connection_requested(self, dest_host: str, dest_port: int, orig_host: str, orig_port: int) -> _NewTCPSession:
+        return True
     
     async def server_requested(self, listen_host: str, listen_port: int) -> MaybeAwait[_NewListener]:
         listener = await self._conn.forward_local_port('', listen_port, '', listen_port)
